ef5ac1582238db585e556aa0daae16a8e1c83a69
Joseph E <1jojorun@gmail.com>
2022-08-11 17:17:48 -0700

    Basic Lexer Generator working with few errors.

--- lexer_generator.py
+++ lexer_generator.py
@@ -1,4 +1,9 @@
+import re
+
+
 class JLex:
+    
+    #####################################################################
     class Rule:
         def __init__(self, regex, resulting_type, value_conversion_method=None):
             self.regex = regex
@@ -6,29 +11,59 @@ class JLex:
             self.value_conversion_method = value_conversion_method
 
         def __repr__(self) -> str:
-            string = f"Rule - regex: '{self.regex}' resulting type: '{self.resulting_type}'"
+            string = f"Rule <regex: '{self.regex}', resulting type: '{self.resulting_type}'>"
             if self.value_conversion_method: 
                 string += f" '{self.value_conversion_method}()'"
             return string
 
+    #####################################################################
+
     class Lexer:
+        ####################################
+        class Token:
+            def __init__(self, type, value):
+                self.type = type
+                self.value = value
+
+            def __repr__(self) -> str:
+                return f"(Token - type: '{self.type}', value: '{self.value}')"
+        ####################################
+
         def __init__(self, rules):
             self.rules = rules
 
-        def generate_tokens():
-            pass
+        def match_rules(self, input:str):
+            for rule in self.rules:
+                match = re.match(rule.regex, input)
+                if match:
+                    value = match.group(0)
+                    return self.Token(rule.resulting_type, value), len(value)
 
-        def print_rules(self):
-            if len(self.rules) == 0:
-                print("There are no rules.")
+            raise Exception('No matches.')
 
-            for i in range(len(self.rules)):
-                print(f'{i+1}:', self.rules[i])
+        def generate_tokens(self, input:str):
+            if len(input) == 0:
+                raise Exception('Input string is empty.')
+
+            token_list = []
+            while len(input) > 0:
+                token, consumed_chars = self.match_rules(input)
+                token_list.append(token)
+                input = input[consumed_chars:len(input)]
+
+            return token_list
 
         def __repr__(self) -> str:
-            return f"Lexer{self.rules}"
+            string = "Lexer<"
+            num_rules = len(self.rules)
+            string += f"\n\tNumber of rules: {num_rules}\n"
+            for i in range(num_rules):
+                string += f'\t{i+1}: {self.rules[i]}'
+                if i < num_rules:
+                    string += '\n'
 
-
+            string += ">"
+            return string
 
     #####################################################################
 
@@ -37,6 +72,7 @@ class JLex:
 
     def create_rule(self, regex:str, resulting_type:str, value_conversion_method=None):
         self.rules.append(self.Rule(regex, resulting_type, value_conversion_method))
+        return self
 
     def get_rules(self):
         return self.rules
@@ -45,8 +81,10 @@ class JLex:
         if len(self.rules) == 0:
             print("There are no rules.")
 
+        string = ''
         for i in range(len(self.rules)):
-            print(f'{i+1}:', self.rules[i])
+            string += f'{i+1}: {self.rules[i]}\n'
+        print(string)
 
     def generate_lexer(self):
         if len(self.rules) == 0:
@@ -57,7 +95,4 @@ class JLex:
 if __name__ == "__main__":
    print("JLex executed when ran directly.")
 else:
-   print("JLex module imported.")
-
-        
-
+   print("JLex module successfully imported.")
--- test.py
+++ test.py
@@ -2,10 +2,18 @@ from lexer_generator import JLex
 
 jlex = JLex()
 
+# Create rules for lexer
 jlex.create_rule("\+", "ADD")
-jlex.create_rule("\d", "INT")
+jlex.create_rule("\-", "SUB")
+jlex.create_rule("\*", "MUL")
+jlex.create_rule("\*", "DIV")
+jlex.create_rule("\d+", "INT")
+jlex.create_rule("(\d)*.(\d+)", "FLOAT")
 
-jlex.print_rules()
-
+# Create lexer object
 lexer = jlex.generate_lexer()
-print(lexer)
+print(f"Lexer object:\n{lexer}")
+
+string = "1+2*3/4"
+tokens = lexer.generate_tokens(string)
+print(tokens)
